7f3fdf314bd3b6af51c577581c897b592d1fd473
moxdev <moxdev1@gmail.com>
2017-06-15 18:02:19 -0400

    mobile front page

--- header.php
+++ header.php
@@ -48,7 +48,7 @@
 			<?php }
 
 			if ( has_nav_menu( 'primary' ) || has_nav_menu( 'aux' ) || has_nav_menu( 'footer' ) ) : ?>
-				<button class="menu-toggle" aria-expanded="false"><?php esc_html_e( 'Primary Menu', 'mm4-you' ); ?></button>
+				<button class="menu-toggle" aria-expanded="false"><?php esc_html_e( 'Menu', 'mm4-you' ); ?></button>
 			<?php endif;  ?>
 
 		</div><!-- .wrapper -->
