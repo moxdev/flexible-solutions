c2c6a66fbfc92a8c5f3213a21eb2167ae4749c3c
moxdev <moxdev1@gmail.com>
2017-06-15 11:57:15 -0400

    rewrote felx-section-2, do not use flex-sections-3

--- functions.php
+++ functions.php
@@ -49,7 +49,8 @@ function mm4_you_setup() {
 	add_image_size('home-highlight-image', 300, 300, true);
 	add_image_size('featured-image', 1400, 360, true);
 	add_image_size('styles-image-square', 500, 500, true);
-	add_image_size('styles-image-wide', 500, 500, true);
+	add_image_size('styles-image-tall', 500, 9999, false);
+	add_image_size('styles-image-custom', 500, 9999, false);
 
 	// This theme uses wp_nav_menu() in one location.
 	register_nav_menus( array(
